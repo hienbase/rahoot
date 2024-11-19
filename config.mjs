b9f37c29fcd8f59a71a46f07553efc3d3fb2fe49
hienphanthanh <154976899+hienphanthanh@users.noreply.github.com>
2024-11-19 06:48:16 +0000

    update

--- config.mjs
+++ config.mjs
@@ -1,5 +1,5 @@
-export const WEBSOCKET_PUBLIC_URL = "http://localhost:5505/"
-export const WEBSOCKET_SERVER_PORT = 5505
+export const WEBSOCKET_PUBLIC_URL = "https://rahoot.vietcombit.vn:5550/"
+export const WEBSOCKET_SERVER_PORT = 5550
 
 const QUIZZ_CONFIG = {
   password: "Brokervang*0",
